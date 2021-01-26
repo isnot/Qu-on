af1bda2360234b0c958da90756648f11301f81c4
ISHIDA Naoto <naoto@isnot.jp>
2021-01-26 23:20:02 +0900

    updateMessage, answerCallbakQuery

--- server/telegram_bot_poll.js
+++ server/telegram_bot_poll.js
@@ -10,14 +10,15 @@ class TelegramBot_poll {
 
   async prepare(player) {
     this.player = player;
-    await this.tg.start(); // .catch(console.err);
-    console.log(new Date(), '[Qu-on] Tg GetUpdateMessageProvider is started');
+    await this.tg.start();
+    console.log('%s [Qu-on] Tg GetUpdateMessageProvider is started', new Date());
   }
 
   async setup() {
     this.tg.setMessageProvider(new telegram.GetUpdateMessageProvider());
 
     this.tg.on('update', (update) => {
+      console.log('DEBUG %o', update);
       const chat_id = update.message.chat.id;
       this.reply_chat_id = chat_id;
       this.sendMessage('DEBUG Message: ' + update.message.text, { chat_id: chat_id });
@@ -32,7 +33,7 @@ class TelegramBot_poll {
   }
 
   async destory() {
-    console.log('[Qu-on] TG will be stopped...' + new Date());
+    console.log('%s [Qu-on] TG will be stopped...', new Date());
     this.tg.stop();
     this.tg = undefined;
   }
@@ -56,9 +57,43 @@ class TelegramBot_poll {
       parse_mode: this.config.parse_mode,
       ...options,
     };
-    // DEBUG console.log(data);
+    console.log('DEBUG', data);
     const response = await this.tg.sendMessage(data);
     console.log('[Qu-on] sendMessage response: %o', response);
+  }
+
+  async updateMessageHTML(chat_id, message_id, text = '', reply_markup) {
+    const config = this.config;
+    const data = {
+      chat_id: chat_id || config.chat_id,
+      message_id: message_id,
+      text: text,
+      disable_web_page_preview: config.disable_web_page_preview,
+      parse_mode: config.parse_mode,
+    };
+    if (!text || text === '') {
+      throw new Error('updateMessageHTML: need text');
+    }
+    if (!message_id) {
+      throw new Error('updateMessageHTML: need message_id');
+    }
+    if (typeof reply_markup === 'string' && reply_markup !== '') {
+      data.reply_markup = reply_markup;
+    } else if (typeof reply_markup === 'object' && reply_markup !== null) {
+      data.reply_markup = JSON.stringify(reply_markup);
+    }
+    return this.tg.editMessageText(data);
+  }
+
+  async answerCallbackQuery(callback_query_id, options = {}) {
+    const data = {
+      callback_query_id: callback_query_id,
+      cache_time: 0,
+      show_alert: true,
+      text: 'acepted!',
+      ...options,
+    };
+    return await this.tg.answerCallbackQuery(data);
   }
 }
 
